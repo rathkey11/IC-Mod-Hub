b458646092b2f5e2229ed7f0437d3c3f26394cf8
rathkey11 <rathkey@yahoo.com>
2024-02-21 23:06:07 -0800

    Enables admin mode

---
+++ Admin-Mode-Enabled/AdminModeEnabled.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using BepInEx;
+using CrusadersGame;
+
+namespace AdminModeEnabled
+{
+    [BepInPlugin("rathkey.ic.adminmodeenabled", "Admin Mode Enabled", "0.1.0")]
+    [BepInProcess("IdleDragons.exe")]
+    public class AdminModeEnabled : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("rathkey.ic.adminmodeenabled");
+
+        void Awake()
+        {
+            harmony.PatchAll(typeof(AdminModeEnabledPatch));
+            Debug.Log("AdminModeEnabled mod loaded"); // Log to check if the mod is loading
+        }
+    }
+
+    [HarmonyPatch(typeof(GameSettings), "ADMIN", MethodType.Getter)]
+    public static class AdminModeEnabledPatch
+    {
+        static bool Prefix(GameSettings __instance, ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
