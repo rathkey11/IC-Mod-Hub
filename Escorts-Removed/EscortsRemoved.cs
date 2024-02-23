2928f668885bba4c26a74f5a52cedf480254b294
rathkey11 <rathkey@yahoo.com>
2024-02-22 20:46:47 -0800

    Removes escorts

---
+++ Escorts-Removed/EscortsRemoved.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using BepInEx;
+using CrusadersGame.GameChanges;
+using static CrusadersGame.GameChanges.GameRules;
+
+namespace EscortsRemoved
+{
+    [BepInPlugin("rathkey.ic.escortsremoved", "Escorts Removed", "0.1.0")]
+    [BepInProcess("IdleDragons.exe")]
+    public class EscortsRemoved : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("rathkey.ic.escortsremoved");
+
+        void Awake()
+        {
+            harmony.PatchAll(typeof(EscortsRemovedPatch));
+            Debug.Log("EscortsRemoved mod loaded"); // Log to check if the mod is loading
+        }
+    }
+
+    [HarmonyPatch(typeof(GameRules), "get_Escorts")]
+    public static class EscortsRemovedPatch
+    {
+        static bool Prefix(ref GameRules.ChangeByArea<EscortInfo> __result)
+        {
+            __result = new GameRules.ChangeByArea<EscortInfo>();
+            return false;
+        }
+    }
+}
