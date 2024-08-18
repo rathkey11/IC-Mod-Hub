15fc2f8a0ce1d8471035b841078f47478a7df01e
rathkey11 <rathkey@yahoo.com>
2024-08-18 00:54:37 -0700

    New for v587

---
+++ Ellywick-Gem-Deck/EllywickGemDeck.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using BepInEx;
+using System.Collections;
+using CrusadersGame.Effects;
+using CrusadersGame.GameScreen;
+
+namespace EllywickGemDeck
+{
+    [BepInPlugin("rathkey.ic.ellywickgemdeck", "Ellywick Gem Deck", "0.1.0")]
+    [BepInProcess("IdleDragons.exe")]
+    public class EllywickGemDeck : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("rathkey.ic.ellywickgemdeck");
+
+        void Awake()
+        {
+            harmony.PatchAll();
+            Debug.Log("EllywickGemDeck loaded");
+        }
+    }
+
+    [HarmonyPatch(typeof(EllywickDeckOfManyThingsHandler), "TryDrawCard")]
+    public static class GemCardsOnly
+    {
+        static bool Prefix(object __instance)
+        {
+            var ultimateActiveField = AccessTools.Field(__instance.GetType(), "ultimateActive");
+            var cardDrawTimerField = AccessTools.Field(__instance.GetType(), "cardDrawTimer");
+            var currentMonsterKillsField = AccessTools.Field(__instance.GetType(), "currentMonsterKills");
+            var cardsInHandField = AccessTools.Field(__instance.GetType(), "cardsInHand");
+            var tryAddCardToHandMethod = AccessTools.Method(__instance.GetType(), "TryAddCardToHand");
+
+            bool ultimateActive = (bool)ultimateActiveField.GetValue(__instance);
+            if (ultimateActive)
+            {
+                return false;
+            }
+
+            var cardDrawTimer = cardDrawTimerField.GetValue(__instance);
+            cardDrawTimer.GetType().GetMethod("Restart").Invoke(cardDrawTimer, null);
+
+            currentMonsterKillsField.SetValue(__instance, 1);
+
+            var cardsInHand = (IList)cardsInHandField.GetValue(__instance);
+            while (cardsInHand.Count < 5)
+            {
+                var cardType = EllywickDeckOfManyThingsHandler.CardType.Gem;
+                tryAddCardToHandMethod.Invoke(__instance, new object[] { cardType, true, false });
+            }
+
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(EllywickDeckOfManyThingsHandler), "MonsterKilled")]
+    public static class OneKillDeckDraw
+    {
+        static bool Prefix(object __instance, ActiveCampaignData e, Monster data)
+        {
+            var tryDrawCardMethod = AccessTools.Method(__instance.GetType(), "TryDrawCard");
+            var cardsInHandField = AccessTools.Field(__instance.GetType(), "cardsInHand");
+
+            var cardsInHand = (IList)cardsInHandField.GetValue(__instance);
+            while (cardsInHand.Count < 5)
+            {
+                tryDrawCardMethod.Invoke(__instance, null);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Ellywick-Gem-Deck/README.md
@@ -0,0 +1,3 @@
+Forces only gem cards to be drawn. Lowers kill to draw to 1. Then loops through this until the full deck is drawn.
+
+Draw animation may show single card draws, but the full deck is drawn on one monster kill.
